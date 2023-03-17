7805cd64d68389530389d7a4d5f1a9785de163e4
mc-b <marcel.bernet@ch-open.ch>
2023-03-17 11:39:17 +0100

    VPN Beispiel, kein full clone

--- main.tf
+++ main.tf
@@ -17,14 +17,27 @@ resource "proxmox_vm_qemu" "vm" {
   memory   = var.memory * 1024
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
+  
+  full_clone = false
 
   ipconfig0 = format("ip=192.168.1.1%s/24,gw=192.168.1.1", split("-", var.module)[1] )
+  # Dynamische IP-Adresse via DHCP
+  # ipconfig0 = "ip=dhcp" 
 
   cicustom = "user=local:snippets/${var.userdata}"
+  
+  /** lernCloud Variante mit setzen Hostname und WireGuard Konfiguration in /opt/wireguard
+  connection {
+    type     = "ssh"
+    host     = self.default_ipv4_address
+    user     = "ubuntu"
+    password = "insecure"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo hostnamectl set-hostname  ${var.module}",
+      "curl -sfL https://raw.githubusercontent.com/mc-b/lerncloud/main/services/vpn.sh | bash -",    
+    ]
+  }  
+  */
 }
-
-
-
-
-
-
